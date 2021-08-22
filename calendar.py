33176d87807fbdf8cf0b922de6bf86e9e11ec999
AndrewMuratov <87572899+AndrewMuratov@users.noreply.github.com>
2021-08-22 15:29:15 -0400

    Delete calendar.py

--- calendar.py
+++
@@ -1,8 +0,0 @@
-import time
-
-class Event:
-    def __init__(self, name, description, time, repeat):
-        self.name = name
-        self.desc = description
-        self.time = time
-        self.rep = repeat
