40419e45c308fe13ae730d0006854213eb339602
Your Name <andrewamuratov@gmail.com>
2021-08-22 13:16:46 -0400

    Created Event Class

---
+++ calendar.py
@@ -0,0 +1,6 @@
+class Event:
+    def __init__(self, name, description, time, repeat):
+        self.name = name
+        self.desc = description
+        self.time = time
+        self.rep = repeat
